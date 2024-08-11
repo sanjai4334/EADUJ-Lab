6e7a0969d530369df3a82b2746c2d4fadfb67208
sanjai4334 <codersanjai@gmail.com>
2024-08-11 21:57:17 +0530

    Added SQL connection

--- EX2 - RMI app/StudentServiceImpl.java
+++ EX2 - RMI app/StudentServiceImpl.java
@@ -1,32 +1,62 @@
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 
 public class StudentServiceImpl extends UnicastRemoteObject implements StudentService {
     private static final long serialVersionUID = 1L;
-    private Map<String, String> students; // Simple storage for demonstration
+
+    // Database connection details
+    private static final String DB_URL = "jdbc:mysql://localhost:9999/studentdb";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "mysql";
 
     protected StudentServiceImpl() throws RemoteException {
         super();
-        students = new HashMap<>();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void addStudent(String rollNum, String name, String mobileNum, int yearOfStudy) throws RemoteException {
-        String studentInfo = "Name: " + name + ", Mobile Number: " + mobileNum + ", Year of Study: " + yearOfStudy;
-        students.put(rollNum, studentInfo);
-        System.out.println("Student added: Roll Number=" + rollNum + ", " + studentInfo);
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String query = "INSERT INTO students (rollNum, name, mobileNum, yearOfStudy) VALUES (?, ?, ?, ?)";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, rollNum);
+            stmt.setString(2, name);
+            stmt.setString(3, mobileNum);
+            stmt.setInt(4, yearOfStudy);
+            stmt.executeUpdate();
+            System.out.println("Student added: Roll Number=" + rollNum + ", Name=" + name + ", Mobile Number=" + mobileNum + ", Year of Study=" + yearOfStudy);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getAllStudents() throws RemoteException {
         StringBuilder sb = new StringBuilder();
-        for (Map.Entry<String, String> entry : students.entrySet()) {
-            sb.append("Roll Number: ").append(entry.getKey())
-              .append(", ").append(entry.getValue())
-              .append("\n");
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String query = "SELECT * FROM students";
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String studentInfo = "Roll Number: " + rs.getString("rollNum") + ", Name: " + rs.getString("name") +
+                        ", Mobile Number: " + rs.getString("mobileNum") + ", Year of Study: " + rs.getInt("yearOfStudy");
+                sb.append(studentInfo).append("\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return sb.toString();
     }
